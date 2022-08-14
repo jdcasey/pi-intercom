730ae47de8077f1a0684ad6ea21ee99ae61c83ce
John Casey <jdcasey@commonjava.org>
2022-08-13 21:48:16 -0500

    Cutting v0.9 version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("requirements.in.txt") as f:
 setup(
     zip_safe=True,
     name='intercompy',
-    version='0.0.1',
+    version='0.9',
     long_description="Turn a Raspberry Pi into an intercom with Telegram",
     classifiers=[
       "Development Status :: 3 - Alpha",
